230fafff6a9fa5be1d5e205ad648600858ff29b8
Yucyj <yousraj2016@gmail.com>
2026-07-15 12:39:42 +0300

    feat: add AuthenticationController with register and login endpoints using Identity

---
+++ ProjectManagement/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using ProjectManagement.Models;
+using ProjectManagement.DTOs;
+
+namespace ProjectManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AuthenticationController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+    }
+}
---
+++ ProjectManagement/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ProjectManagement.Models
+{
+    // This connects your project to Mohamed's Identity structure
+    public class ApplicationUser : IdentityUser
+    {
+        // If your team wants to link a User to an Employee, they might add this line later:
+        // public int? EmployeeId { get; set; }
+        // public Employee Employee { get; set; }
+    }
+}
