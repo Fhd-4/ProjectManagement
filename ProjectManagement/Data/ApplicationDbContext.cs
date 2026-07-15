43d428cc3c0928572e364c942d41316e83e62b89
Yucyj <yousraj2016@gmail.com>
2026-07-15 15:17:20 +0300

    users api

--- ProjectManagement/Data/ApplicationDbContext.cs
+++ ProjectManagement/Data/ApplicationDbContext.cs
@@ -1,17 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ProjectManagement.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
 
 namespace ProjectManagement.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
+
     {
-        // هذا الكونستركتور يمرر إعدادات الاتصال لقاعدة البيانات
+     
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
-
-        // هنا نقول له كرت لنا جدول اسمه Employees بناءً على كلاس الـ Employee
         public DbSet<Employee> Employees { get; set; }
     }
 }
