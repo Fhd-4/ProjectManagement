d2458224bb0dd3612ce2b61e87f1a7e295ba0d19
Yucyj <yousraj2016@gmail.com>
2026-07-16 11:08:00 +0300

    commit

--- ProjectManagement/Models/ApplicationUser.cs
+++ ProjectManagement/Models/ApplicationUser.cs
@@ -2,11 +2,10 @@
 
 namespace ProjectManagement.Models
 {
-    // This connects your project to Mohamed's Identity structure
     public class ApplicationUser : IdentityUser
     {
-        // If your team wants to link a User to an Employee, they might add this line later:
-        // public int? EmployeeId { get; set; }
-        // public Employee Employee { get; set; }
+        public string? ResetOtp { get; set; }
+
+        public DateTime? ResetOtpExpiry { get; set; }
     }
 }
